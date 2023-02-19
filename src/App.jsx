285e559088db3e7d622cb38b0e8b8eb093e0890b
namdar12 <namdar.mesri12@gmail.com>
2023-02-18 21:33:33 -0800

    added some styling to the button

--- src/App.jsx
+++ src/App.jsx
@@ -29,13 +29,7 @@ function App() {
     setResults([]);
     setHasQueried(false);
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    // MetaMask requires requesting permission to connect users accounts
     await provider.send("eth_requestAccounts", []);
-
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...
     const signer = await provider.getSigner();
     const signerAddress = await signer.getAddress();
     setUserAddress(signerAddress);
